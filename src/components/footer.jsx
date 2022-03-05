83a3ec19dcc4c23fdad8e251f83781d57108dffd
sangchu <sangchu9704@gmail.com>
2022-03-05 15:54:11 +0900

    footer for mobile/desktop update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -24,15 +24,23 @@ const FootP1 = styled.p`
   }
 `;
 
-const FootP2 = styled.p`
+const FootP2Desktop = styled.p`
   font-size: 0.8em;
   color: #666666;
-  padding-top: 18px;
+  padding-top: 20px;
   line-height: 200%;
   @media ${({ theme }) => theme.device.mobile} {
-    line-height: 1.3;
-    padding-top: 10px;
-    font-size: 0.7rem;
+    display: none;
+  }
+`;
+
+const FootP2Mobile = styled.p`
+  color: #666666;
+  line-height: 150%;
+  padding-top: 20px;
+  font-size: 0.7rem;
+  @media ${({ theme }) => theme.device.desktop} {
+    display: none;
   }
 `;
 
@@ -71,12 +79,18 @@ const footer = () => {
     <Footerbar>
       <div style={{ width: '82.91vw', borderTop: '1px solid #bcbcbc' }}>
         <FootP1>인진지코리아</FootP1>
-        <FootP2>
+        <FootP2Desktop>
           대표자: 박원민 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;주소: 경기도 고양시
           일산서구 일현로 159 4층 좌측 injinji
           <br></br>사업자등록번호: 659-55-00516&nbsp;통신판매업신고:
           2019-고양일산서-0871
-        </FootP2>
+        </FootP2Desktop>
+        <FootP2Mobile>
+          대표자: 박원민<br></br>주소: 경기도 고양시 일산서구 일현로 159 4층
+          좌측 injinji
+          <br></br>사업자등록번호: 659-55-00516<br></br>통신판매업신고:
+          2019-고양일산서-0871
+        </FootP2Mobile>
         <FootSpan1>고객센터</FootSpan1>
         <FootSpan2>070-8095-2374</FootSpan2>
         <FootP3>운영시간 : 09:00 ~ 18:00</FootP3>
