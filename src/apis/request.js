dfd7d5f68438aa9d566eaa5304f54c87c16c9dea
qq8721443 <qq8721443@naver.com>
2022-07-05 01:28:12 +0900

    refactor: try catch 구문 제거

--- src/apis/request.js
+++ src/apis/request.js
@@ -1,15 +1,8 @@
 import { HTTP_URL } from '../constants';
 
 export const request = async (url, options) => {
-  try {
-    const res = await fetch(`${HTTP_URL}${url}`, {
-      ...options,
-    });
-    if (!res.ok) {
-      throw new Error('API 호출 중 문제가 발생했습니다.');
-    }
-    return await res.json();
-  } catch (e) {
-    console.log(e);
-  }
+  const res = await fetch(`${HTTP_URL}${url}`, {
+    ...options,
+  });
+  return await res.json();
 };
